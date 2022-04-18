15609bd76f7b98e6cb3d19e163e7889c6c0585b5
Will Young <will@Wills-Mac-mini.local>
2022-04-17 19:11:36 -0500

    test: add integration tests using jest and supertest

--- controller/request.js
+++ controller/request.js
@@ -10,11 +10,11 @@ module.exports = {
         //Github API Request to get list of PRs
         axios.get(url).then( async response => {
 
-            data = response.data
+            let data = response.data
 
             // Loop to iterate over data and build object to push into the jsonArr array
             for (let i=0;i<data.length; i++) {
-            number = data[i].number
+            let number = data[i].number
             //Github API Request to list number of commits for each Open PR
             const commits = await axios.get(`${url}/${number}/commits`)
             let newItem = {
@@ -27,11 +27,11 @@ module.exports = {
             
                 jsonArr.push(newItem)
             }
-            
             res.status(200).json(jsonArr)
-            console.log(jsonArr)
         }).catch(err => {
-            res.status(500).send("Github repository is either private or doesn't exist.")
+            res.status(500).send({
+                error: `This repository (${repo}) by ${username} either has no open pull requests, is private or doesn't exist.`
+            })
         })
 
     }
